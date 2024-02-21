4ebc2e95b245dd3bad7abcb8eeb538992aa6073b
yuhuajing <18852897525@163.com>
2024-02-21 18:17:54 +0800

    add page

--- main.go
+++ main.go
@@ -9,11 +9,33 @@ import (
 
 func main() {
 	app := fiber.New()
+	app.Static("/login", "./form.html")
 	app.Post("/", func(c *fiber.Ctx) error {
 		fmt.Println(string(c.Body()))
 		//return c.SendString("POST request")
 		return c.SendStatus(200)
 	})
 
-	log.Fatal(app.Listen(":3010"))
+	log.Fatal(app.Listen(":3000"))
 }
+
+//func GetNFTData(c *fiber.Ctx) error {
+//	chain := c.Query("chain")
+//	contract := c.Query("contract")
+//	collection := c.Query("collection")
+//	err, res := mongodb.QueryNFTdataDB(config.Dbcollectionnftdata, chain, contract, collection)
+//	if err != nil {
+//		return c.Status(401).JSON(types.ErrorResponse{
+//			Error:   err.Error(),
+//			Success: false,
+//			Data:    "",
+//		})
+//	}
+//
+//	return c.Status(200).JSON(types.ResResponse{
+//		Error:   "",
+//		Success: true,
+//		Data:    res,
+//	})
+//	//return handler.SendResponse(c, res)
+//}
